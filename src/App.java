a2801f751d3dad9a396138b411393db5985400a4
Felipe Paixão <95901427+felipepaixao16@users.noreply.github.com>
2023-04-01 22:49:43 -0300

    bd

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
 // Consumindo uma API de Filmes da IMDB
 
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -19,28 +14,34 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 3 filmes
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-        String url = "https://api.nasa.gov/planetary/apod?api_key=spBQEUdMeKSoHAZN73evOmuKfiIzqDpzTWpanKrF&start_date=2022-05-12&end_date=2022-05-14";
+        //ExtratorDeConteudoIMDB extrator = new ExtratorDeConteudoIMDB();
 
-        // pegar só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeConteudos = parser.parse(body);
+
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=spBQEUdMeKSoHAZN73evOmuKfiIzqDpzTWpanKrF&start_date=2022-05-12&end_date=2022-05-14";
+        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+
+        String url = "http://localhost:8080/linguagens";
+        ExtratorDeConteudoIMDB extrator = new ExtratorDeConteudoIMDB();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+        
 
         // exibir e manipular os dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
+
         for (int i = 0; i < 3; i++) {
 
-            Map<String,String> conteudo = listaDeConteudos.get(i);
+            Conteudo conteudo = conteudos.get(i);
 
-            String urlImagem = conteudo.get("url").replaceAll("(@+)(.*).jpg$", "$1.jpg");
-
-            String titulo = conteudo.get("title");
-
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "C:/Users/Felip/Desktop/alura/apiStikers-java/saida/" + titulo + ".png";
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "C:/Users/Felip/Desktop/alura/apiStikers-java/saida/" + conteudo.titulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(titulo);
+            System.out.println(conteudo.titulo());
             System.out.println();
 
             // transformando uma String em Int.
